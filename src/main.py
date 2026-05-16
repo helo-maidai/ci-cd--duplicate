e2e318f5133b896be217a88c14a4fff9177b65e9
Xin Yi <lewxinyi@graduate.utm.my>
2026-05-16 07:51:34 +0000

    Add duplicate removal logic and automated test

---
+++ src/main.py
@@ -0,0 +1,16 @@
+
+import pandas as pd
+
+def load_and_process_data(filepath = "data/dataset.csv", output_path = "data/processed_dataset.csv"):
+  
+  #Load dataset, remove duplicates & save the processed dataset
+  df = pd.read_csv(filepath)
+  print(f"Original dataset shape: {df.shape}")
+
+  df = df.drop_duplicates()
+  print(f"Dataset shape after removing duplicates")
+
+  df.to_csv(output_path, index=False)
+  print(f"Processed dataset saved to {output_path}")
+
+  return df
---
+++ tests/test_main.py
@@ -0,0 +1,17 @@
+
+import sys
+import os
+
+# Add src folder to py path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
+from main import load_and_process_data
+
+def test_no_duplicates():
+
+  df = load_and_process_data("data/dataset.csv", "data/test_processed_dataset.csv")
+
+  assert not df.duplicated().sum() == 0, "Duplicated rows were not fully removed"
+
+# Run test locally
+!pytest tests/ -v
