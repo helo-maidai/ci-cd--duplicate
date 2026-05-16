c79ff58cac4793b648f029e82ae139753c972e7a
Xin Yi <lewxinyi@graduate.utm.my>
2026-05-16 10:06:53 +0000

    Add duplicate removal logic and automated test

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 def load_and_process_data(filepath = "data/dataset.csv", output_path = "data/processed_dataset.csv"):
-  
+
   #Load dataset, remove duplicates & save the processed dataset
   df = pd.read_csv(filepath)
   print(f"Original dataset shape: {df.shape}")
